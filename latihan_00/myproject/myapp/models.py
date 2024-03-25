e76525cfb9bac77c4e808fb57216d76da5ff2295
Haloio29 <aureliolucio5@gmail.com>
2024-03-25 11:50:21 +0700

    Add class Album, Song, Book, Author

--- latihan_00/myproject/myapp/models.py
+++ latihan_00/myproject/myapp/models.py
@@ -17,3 +17,31 @@ class JobTitle(models.Model):
                             on_delete=models.SET_NULL)
   def __str__(self):
     return self.name
+class Album(models.Model):
+  title = models.CharField(max_length=100)
+  artist = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.title
+  
+class Song(models.Model):
+  title = models.CharField(max_length=100)
+  album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
+  
+class Author(models.Model):
+  name = models.CharField(max_length=100)
+  desc = models.TextField(max_length=300)
+
+  def __str__(self):
+    return self.name
+  
+class Book(models.Model):
+  title = models.CharField(max_length=100)
+  desc = models.TextField(max_length=300)
+  authors = models.ManyToManyField(Author, related_name='book_author')
+
+  def __str__(self):
+    return self.title
