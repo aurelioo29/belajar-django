0768c74a4cb2c33c6523eda1014199818c01f8d8
Haloio29 <aureliolucio5@gmail.com>
2024-03-25 11:51:12 +0700

    Add import albung,song,author,book from models

--- latihan_00/myproject/myapp/admin.py
+++ latihan_00/myproject/myapp/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from myapp.models import StatusModel
 
 # Register your models here.
+from .models import Album, Song, Author, Book
+
 admin.site.register(StatusModel)
+# admin.site.register(GeeksModel)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Author)
+admin.site.register(Book)
